85864c8f3372ea60d68dc1a49808162b46cc0a80
Rafaelmkp <rafaelmobarack@gmail.com>
2020-11-07 14:21:32 -0300

    commit drago

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # RitterFlix
 
 Application developed for Android using Kotlin with MVVM architecture, Live Data and Data Binding.
-
-[Relatório de entrega A1](/Relatorio1.md)
--- app/src/main/java/com/dispositivosmoveis/ritterflix/ui/home/HomeActivity.kt
+++ app/src/main/java/com/dispositivosmoveis/ritterflix/ui/home/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.dispositivosmoveis.ritterflix.ui.home
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import androidx.fragment.app.Fragment
 import com.dispositivosmoveis.ritterflix.R
 import com.dispositivosmoveis.ritterflix.repository.models.Movie
 import com.dispositivosmoveis.ritterflix.repository.models.ReleasedMovie
 import com.dispositivosmoveis.ritterflix.ui.detail.MovieDetailFragment
+import com.synnapps.carouselview.CarouselView
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -19,6 +27,28 @@ class HomeActivity : AppCompatActivity() {
                 .add(R.id.home_container, HomeFragment.newInstance(), "homeFragment")
                 .commit()
         }
+
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val moreFragment = MoreFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.ic_home_nav -> makeCurrentFragment(homeFragment)
+                R.id.ic_search_nav -> makeCurrentFragment(searchFragment)
+                R.id.ic_more_nav -> makeCurrentFragment(moreFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.home_container, fragment)
+            commit()
     }
 
     override fun onBackPressed() {
